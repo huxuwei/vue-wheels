4061ae71b3da9393efbeaedb8ebb5c7b7230e79f
hu <hu_xuwei@163.com>
2018-06-30 17:36:29 +0800

    button初始化

---
+++ src/button.vue
@@ -0,0 +1,59 @@
+<template>
+    <button class="g-button" :class="{[iconPosition]:true}"
+    @click="$emit('click')">
+        <g-icon class="icon" :icon='icon' v-if="icon"></g-icon>
+        <g-icon class="icon" icon='loading' v-if="loading"></g-icon>
+        <span>{{label}}</span>
+    </button>
+</template>
+
+<script>
+export default {
+    props:{
+        label: {
+            type: String,
+            default: '按钮'
+        },
+        icon:{
+            type: String
+        },
+        iconPosition:{
+            type: String,
+            default: 'left',
+            validator:(v)=>{
+                return v ==='right' || v ==='left'
+            }
+        },
+        disabled: Boolean,
+        loading: Boolean
+    },
+    
+}
+</script>
+
+<style lang='scss'>
+.g-button{
+    height: 32px;
+    background: var(--button-bg);
+    border:1px solid var(--border-color);
+    border-radius: var(--border-radius);
+    padding: 0 1em;
+    vertical-align: middle;
+    display: inline-flex;
+    align-items: center;
+    &:hover{
+        cursor: pointer;
+        border-color: var(--border-active-color)
+    }
+    &:active{
+        background: var(--button-active-bg);
+    }
+    .icon{
+        margin-right: .2em;
+    }
+    &.right{
+        >.icon{ order: 1 ;margin-left:.2em; margin-right: 0;}
+    }
+}
+
+</style>
---
+++ src/icon.vue
@@ -0,0 +1,30 @@
+<template>
+    <svg class="icon" aria-hidden="true">
+        <use :xlink:href='iconS'></use>
+    </svg>
+</template>
+
+<script>
+export default {
+    props:{
+        icon: {
+            type: String,
+        }
+    },
+    computed:{
+        iconS(){
+            return `#i-${this.icon}`
+        }
+    }
+}
+</script>
+
+<style>
+.icon {
+       width: 1em; height: 1em;
+       /* vertical-align: -0.15em; */
+       /* fill: currentColor;
+       overflow: hidden;
+       vertical-align: middle */
+    }
+</style>
