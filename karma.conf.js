4d80d48b04c9c252e68314168d3de633bff4f8d3
hu <hu_xuwei@163.com>
2018-07-05 09:41:36 +0800

    利用tranvisCI 进行持续集成测试

--- karma.conf.js
+++ karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function (config) {
 
        // start these browsers
        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-       browsers: ['Chrome'],
+       browsers: ['ChromeHeadless'],
        browserNoActivityTimeout:50000,
        browserDisconnectTimeout:50000,
        // Continuous Integration mode
