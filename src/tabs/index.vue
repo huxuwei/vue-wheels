0b82c5b21b2b21cc2e2274e23a0a13fe950794b2
unknown <563384409@qq.com>
2019-01-14 18:01:19 +0800

    init tabs

---
+++ src/tabs/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="g-tab-wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import tabPane from './tab-pane'
+export default {
+  components: {
+    tabPane
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
