d611d9736e1a4efc24849897236fb77f887b98bf
hu <563384409@qq.com>
2019-01-29 14:56:56 +0800

    toast添加自动关闭

--- src/plugin.js
+++ src/plugin.js
@@ -2,11 +2,10 @@ import Toast from './toast'
 
 export default {
   install(Vue, Options) {
-    // console.log(Vue)
     Vue.prototype.$toast =  (message)=>{
       let a = Vue.extend(Toast)
       let b = new a()
-      console.log(b)
+      
       b.$slots.default = [message]
       b.$mount()
       document.body.appendChild(b.$el)
--- src/toast.vue
+++ src/toast.vue
@@ -6,18 +6,43 @@
 
 <script>
 export default {
-
+  props: {
+    autoClose: {
+      type: Boolean,
+      default: true
+    },
+    autoCloseDelay: {
+      type: Number,
+      default: 5
+    }
+  },
+  mounted() {
+    if(this.autoClose) {
+      setTimeout(() => {
+        this.close()
+      }, this.autoCloseDelay * 1000)
+    }
+  },
+  methods: {
+    close() {
+      this.$el.remove()
+      this.$destroy()
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-$toast-background-color: #333;
+$toast-background-color: grey;
+$toast-color: #ccc;
 .toast{
   position: fixed;
-
+  padding: 6px 10px;
   top: 10px;
   left: 50%;
-  transform: translateX(-50%)
+  transform: translateX(-50%);
+  background-color: $toast-background-color;
+  color: $toast-color;
 }
 </style>
 
