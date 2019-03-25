3ac293b1a9e85eae391b0bc4053d6ffdd9de0607
unknown <563384409@qq.com>
2019-03-25 13:40:18 +0800

    修改toast

--- src/toast.vue
+++ src/toast.vue
@@ -6,18 +6,33 @@
 
 <script>
 export default {
-
+  
 }
 </script>
 
 <style lang="scss" scoped>
-$toast-background-color: #333;
+@import '@/style/style.scss';
 .toast{
   position: fixed;
-
   top: 10px;
   left: 50%;
-  transform: translateX(-50%)
+  transform: translateX(-50%);
+}
+.success {
+  background-color: $background-color-success;
+  color: $color-success;
+}
+.info{
+  background-color: $background-color-info;
+  color: $color-info;
+}
+.warning{
+  background-color: $background-color-warning;
+  color: $color-warning;
+}
+.danger {
+  background-color: $background-color-danger;
+  color: $color-danger;
 }
 </style>
 
