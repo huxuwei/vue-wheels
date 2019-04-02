cf94b567dee1d1fbdfe4a8ff01ec658c3c6f105e
unknown <563384409@qq.com>
2019-04-02 16:18:19 +0800

    编辑docs

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
+
+[项目文档][docs]
 ## 介绍
 - 个人开发的基于 Vue 2 的UI库
 - 注意：本 UI 框架的代码尚未完全完工,请不要在生产环境中使用本 UI 框架,欢迎讨论交流
 
 
+[docs]: http://localhost:8080/vue-wheels/install/
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,19 @@
 const path = require('path')
 module.exports = {
   base:'/vue-wheels/',
-  title: 'Hello VuePress1',
+  title: 'vue-wheels',
   description: 'Just playing around',
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
+      { text: 'GitHub', link: 'https://github.com/huxuwei/vue-wheels' },
     ],
     sidebar: [
       {
         title: "入门",
         collapsable: false,
         children: [
-          ["/install/", "安装"]
+          ["/install/", "介绍"]
         ]
       },
       {
@@ -21,9 +22,9 @@ module.exports = {
         children: [
           ["/components/tag",'tag'],
           ["/components/button",'button'],
-          ["/components/input",'input'],
           ["/components/card",'card'],
           ["/components/tabs",'tabs'],
+          ["/components/input",'input'],
           ["/components/toast",'toast'],
           ["/components/popover",'popover']
         ]
